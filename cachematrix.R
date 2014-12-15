67edbde5a90c5d980f48d6ef2f783148a4fd2045
Deminish <mike7826@gmail.com>
2014-12-15 15:35:02 +0000

    Framework of script
    Added the framework of the script based on the Vector Mean example
    script given

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,18 @@
 ## Create a special matrix that can cache it's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setInverse function(inverse) m <<- inverse
+        getInverse <- function() m
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
+        
 }
 
 
@@ -15,4 +26,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getInverse()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- inverse(data, ...)
+        x$setInverse(m)
+        m
 }
